64678a866cbb5cc52a7815786202f29a02e918d9
MichaeLOl1v <125513389+MichaeLOl1v@users.noreply.github.com>
2023-10-06 21:53:45 -0300

    Add files via upload

---
+++ includes/Constants.php
@@ -0,0 +1,8 @@
+<?php 
+
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'Greenmap');
+	define('DB_PASS', '123456');
+	define('DB_NAME', 'vegmap');
+
+ ?>
---
+++ includes/DbOperation.php
@@ -0,0 +1,30 @@
+<?php 
+	
+class DbOperation
+{
+
+	private $conexao;
+	
+	function __construct()
+	{
+
+		require_once dirname(__FILE__) . 'DbConnect.php';
+
+		$db = new DbConnect();
+
+		$this->conexao = $db->connect();
+	}
+
+
+	function createusuario($nome, $telefone, $email, $senha){
+		$stmt = $this->conexao->prepare("INSERT INTO usuario (nome, telefone, email, senha) VALUES (?, ?, ?, ?)");
+		$stmt ->execute();
+		$stmt
+	}
+}
+
+
+
+
+
+ ?>
