6b17f8e20632b4f58b9c2cbf90f2b2bbd0a19ffe
MichaeLOl1v <125513389+MichaeLOl1v@users.noreply.github.com>
2023-10-30 22:02:17 -0300

    Add files via upload

--- v1/Api.php
+++ v1/Api.php
@@ -407,4 +407,400 @@
 	echo json_encode($response);
 
 
+	// PRODUTO
+
+
+	function isTheseParametersAvailable($params){
+	
+		$available = true; 
+		$missingparams = ""; 
+		
+		foreach($params as $param){
+			if(!isset($_POST[$param]) || strlen($_POST[$param])<=0){
+				$available = false; 
+				$missingparams = $missingparams . ", " . $param; 
+			}
+		}
+		
+		
+		if(!$available){
+			$response = array(); 
+			$response['error'] = true; 
+			$response['message'] = 'Parameters ' . substr($missingparams, 1, strlen($missingparams)) . ' missing';
+			
+		
+			echo json_encode($response);
+			
+		
+			die();
+		}
+	}
+	
+	
+	$response = array();
+	
+
+	if(isset($_GET['apicall'])){
+		
+		switch($_GET['apicall']){
+	
+			case 'createproduto':
+				
+				isTheseParametersAvailable(array('nomeprodu','tipoprodu','medidaprodu','saborprodu'));
+				
+				$db = new DbOperation();
+				
+				$result = $db->createproduto(
+					$_POST['nomeprodu'],
+					$_POST['tipoprodu'],
+					$_POST['medidaprodu'],
+					$_POST['saborprodu']
+				);
+				
+
+			
+				if($result){
+					
+					$response['error'] = false; 
+
+					
+					$response['message'] = 'Produto adicionado com sucesso';
+
+					
+					$response['produtos'] = $db->getprodutos();
+				}else{
+
+					
+					$response['error'] = true; 
+
+				
+					$response['message'] = 'Algum erro ocorreu por favor tente novamente';
+				}
+				
+			break; 
+			
+		
+			case 'getprodutos':
+				$db = new DbOperation();
+				$response['error'] = false; 
+				$response['message'] = 'Pedido concluído com sucesso';
+				$response['produtos'] = $db->getprodutos();
+			break; 
+			
+			
+		
+			case 'updateproduto':
+				isTheseParametersAvailable(array('idprodu','nomeprodu','tipoprodu','medidaprodu','saborprodu'));
+				$db = new DbOperation();
+				$result = $db->updateproduto(
+					$_POST['idprodu'],
+					$_POST['nomeprodu'],
+					$_POST['tipoprodu'],
+					$_POST['medidaprodu'],
+					$_POST['saborprodu']
+				);
+				
+				if($result){
+					$response['error'] = false; 
+					$response['message'] = 'Produto atualizado com sucesso';
+					$response['produtos'] = $db->getprodutos();
+				}else{
+					$response['error'] = true; 
+					$response['message'] = 'Algum erro ocorreu por favor tente novamente';
+				}
+			break; 
+			
+			
+			case 'deleteproduto':
+
+				
+				if(isset($_GET['idprodu'])){
+					$db = new DbOperation();
+					if($db->deleteusuario($_GET['idprodu'])){
+						$response['error'] = false; 
+						$response['message'] = 'Produto excluído com sucesso';
+						$response['produtos'] = $db->getprodutos();
+					}else{
+						$response['error'] = true; 
+						$response['message'] = 'Algum erro ocorreu por favor tente novamente';
+					}
+				}else{
+					$response['error'] = true; 
+					$response['message'] = 'Não foi possível deletar, forneça um id por favor';
+				}
+			break; 
+		}
+		
+	}else{
+		 
+		$response['error'] = true; 
+		$response['message'] = 'Chamada de API inválida';
+	}
+	
+
+	echo json_encode($response);
+
+
+	// AVALIAÇÃO
+
+
+	function isTheseParametersAvailable($params){
+	
+		$available = true; 
+		$missingparams = ""; 
+		
+		foreach($params as $param){
+			if(!isset($_POST[$param]) || strlen($_POST[$param])<=0){
+				$available = false; 
+				$missingparams = $missingparams . ", " . $param; 
+			}
+		}
+		
+		
+		if(!$available){
+			$response = array(); 
+			$response['error'] = true; 
+			$response['message'] = 'Parameters ' . substr($missingparams, 1, strlen($missingparams)) . ' missing';
+			
+		
+			echo json_encode($response);
+			
+		
+			die();
+		}
+	}
+	
+	
+	$response = array();
+	
+
+	if(isset($_GET['apicall'])){
+		
+		switch($_GET['apicall']){
+	
+			case 'createavaliacao':
+				
+				isTheseParametersAvailable(array('nota','comentario','idestab'));
+				
+				$db = new DbOperation();
+				
+				$result = $db->createavaliacao(
+					$_POST['nota'],
+					$_POST['comentario'],
+					$_POST['idestab'],
+				);
+				
+
+			
+				if($result){
+					
+					$response['error'] = false; 
+
+					
+					$response['message'] = 'Avaliação adicionado com sucesso';
+
+					
+					$response['avaliacaos'] = $db->getavaliacaos();
+				}else{
+
+					
+					$response['error'] = true; 
+
+				
+					$response['message'] = 'Algum erro ocorreu por favor tente novamente';
+				}
+				
+			break; 
+			
+		
+			case 'getavaliacaos':
+				$db = new DbOperation();
+				$response['error'] = false; 
+				$response['message'] = 'Pedido concluído com sucesso';
+				$response['avaliacaos'] = $db->getavaliacaos();
+			break; 
+			
+			
+		
+			case 'updateavaliacao':
+				isTheseParametersAvailable(array('idavaliacao','nota','comentario','idestab'));
+				$db = new DbOperation();
+				$result = $db->updateavaliacao(
+					$_POST['idavaliacao'],
+					$_POST['nota'],
+					$_POST['comentario'],
+					$_POST['idestab']
+				);
+				
+				if($result){
+					$response['error'] = false; 
+					$response['message'] = 'Produto atualizado com sucesso';
+					$response['avaliacaos'] = $db->getavaliacaos();
+				}else{
+					$response['error'] = true; 
+					$response['message'] = 'Algum erro ocorreu por favor tente novamente';
+				}
+			break; 
+			
+			
+			case 'deleteavaliacao':
+
+				
+				if(isset($_GET['idavaliacao'])){
+					$db = new DbOperation();
+					if($db->deleteusuario($_GET['idavaliacao'])){
+						$response['error'] = false; 
+						$response['message'] = 'Avaliação excluído com sucesso';
+						$response['avaliacaos'] = $db->getavaliacaos();
+					}else{
+						$response['error'] = true; 
+						$response['message'] = 'Algum erro ocorreu por favor tente novamente';
+					}
+				}else{
+					$response['error'] = true; 
+					$response['message'] = 'Não foi possível deletar, forneça um id por favor';
+				}
+			break; 
+		}
+		
+	}else{
+		 
+		$response['error'] = true; 
+		$response['message'] = 'Chamada de API inválida';
+	}
+	
+
+	echo json_encode($response);
+
+
+	// AVALIAÇÃO P
+
+
+	function isTheseParametersAvailable($params){
+	
+		$available = true; 
+		$missingparams = ""; 
+		
+		foreach($params as $param){
+			if(!isset($_POST[$param]) || strlen($_POST[$param])<=0){
+				$available = false; 
+				$missingparams = $missingparams . ", " . $param; 
+			}
+		}
+		
+		
+		if(!$available){
+			$response = array(); 
+			$response['error'] = true; 
+			$response['message'] = 'Parameters ' . substr($missingparams, 1, strlen($missingparams)) . ' missing';
+			
+		
+			echo json_encode($response);
+			
+		
+			die();
+		}
+	}
+	
+	
+	$response = array();
+	
+
+	if(isset($_GET['apicall'])){
+		
+		switch($_GET['apicall']){
+	
+			case 'createavaliacaop':
+				
+				isTheseParametersAvailable(array('notap','comentariop','idprodu'));
+				
+				$db = new DbOperation();
+				
+				$result = $db->createavaliacaop(
+					$_POST['notap'],
+					$_POST['comentariop'],
+					$_POST['idprodu'],
+				);
+				
+
+			
+				if($result){
+					
+					$response['error'] = false; 
+
+					
+					$response['message'] = 'Avaliação adicionado com sucesso';
+
+					
+					$response['avaliacaosp'] = $db->getavaliacaosp();
+				}else{
+
+					
+					$response['error'] = true; 
+
+				
+					$response['message'] = 'Algum erro ocorreu por favor tente novamente';
+				}
+				
+			break; 
+			
+		
+			case 'getavaliacaosp':
+				$db = new DbOperation();
+				$response['error'] = false; 
+				$response['message'] = 'Pedido concluído com sucesso';
+				$response['avaliacaosp'] = $db->getavaliacaosp();
+			break; 
+			
+			
+		
+			case 'updateavaliacaop':
+				isTheseParametersAvailable(array('idavaliacaop','notap','comentariop','idprodu'));
+				$db = new DbOperation();
+				$result = $db->updateavaliacaop(
+					$_POST['idavaliacaop'],
+					$_POST['notap'],
+					$_POST['comentariop'],
+					$_POST['idprodu']
+				);
+				
+				if($result){
+					$response['error'] = false; 
+					$response['message'] = 'Avaliaçãop atualizado com sucesso';
+					$response['avaliacaosp'] = $db->getavaliacaosp();
+				}else{
+					$response['error'] = true; 
+					$response['message'] = 'Algum erro ocorreu por favor tente novamente';
+				}
+			break; 
+			
+			
+			case 'deleteavaliacaop':
+
+				
+				if(isset($_GET['idavaliacaop'])){
+					$db = new DbOperation();
+					if($db->deleteusuario($_GET['idavaliacaop'])){
+						$response['error'] = false; 
+						$response['message'] = 'Avaliação excluído com sucesso';
+						$response['avaliacaosp'] = $db->getavaliacaosp();
+					}else{
+						$response['error'] = true; 
+						$response['message'] = 'Algum erro ocorreu por favor tente novamente';
+					}
+				}else{
+					$response['error'] = true; 
+					$response['message'] = 'Não foi possível deletar, forneça um id por favor';
+				}
+			break; 
+		}
+		
+	}else{
+		 
+		$response['error'] = true; 
+		$response['message'] = 'Chamada de API inválida';
+	}
+	
+
+	echo json_encode($response);
 ?>
