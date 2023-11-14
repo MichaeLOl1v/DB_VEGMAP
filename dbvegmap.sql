3e87da7d26752957a62332daf68e0e4abf60956c
MichaeLOl1v <125513389+MichaeLOl1v@users.noreply.github.com>
2023-11-14 19:24:56 -0300

    Add files via upload

--- dbvegmap.sql
+++ dbvegmap.sql
@@ -5,7 +5,7 @@ USE vegmap;
 CREATE TABLE usuario(
 idusuario TINYINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 nome VARCHAR(30) NOT NULL,
-telefone TINYINT NOT NULL,
+telefone VARCHAR(15) NOT NULL,
 email VARCHAR(50) NOT NULL,
 senha VARCHAR(25) NOT NULL
 );
@@ -14,7 +14,7 @@ CREATE TABLE usuariopc(
 idusuarioPC TINYINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 nomePC VARCHAR(30) NOT NULL,
 cnpj INT NOT NULL,
-telefonePC TINYINT NOT NULL,
+telefonePC VARCHAR(15) NOT NULL,
 emailPC VARCHAR(50) NOT NULL,
 senhaPC VARCHAR(25) NOT NULL
 );
@@ -23,7 +23,7 @@ CREATE TABLE estabelecimento(
 idestab TINYINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 nome_estab VARCHAR(30) NOT NULL,
 endereco VARCHAR(40) NOT NULL,
-telefone_estab TINYINT NOT NULL,
+telefone_estab VARCHAR(15) NOT NULL,
 descricaoestab VARCHAR(320) NOT NULL,
 idusuarioPC TINYINT NOT NULL,
 tipo VARCHAR(20) NOT NULL,
@@ -56,3 +56,6 @@ idprodu TINYINT NOT NULL,
 
 FOREIGN KEY (idProdu) REFERENCES produto (idProdu)
 );
+
+INSERT INTO usuario (idusuario, nome, telefone, email, senha)
+VALUES (NULL, 'Mike', '11971653287', '@MIKE', '123');
